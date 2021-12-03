e0bf5c32849febe694a4d8efbb200a73d072d590
FahrizalDR <frinaldi@student.ciputra.ac.id>
2021-12-03 10:34:19 +0700

    program mengecek bilangan prima

--- Latihan Repository/Program.cs
+++ Latihan Repository/Program.cs
@@ -6,7 +6,30 @@ namespace Latihan_Repository
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Silahkan masukkan angka : ");
+            int angka = Convert.ToInt32(Console.ReadLine());
+            int jumlahbilangan = 0;
+            int sisa;
+            for (int i=1; i <= angka;i++)
+            {
+                sisa = angka % i;
+                if (sisa == 0)
+                {
+                    jumlahbilangan += 1;
+                }
+                else
+                {
+                    jumlahbilangan = jumlahbilangan;
+                }
+            }
+            if (jumlahbilangan > 2)
+            {
+                Console.Write($"Angka {angka} adalah bukan bilangan prima");
+            }
+            else
+            {
+                Console.Write($"Angka {angka} adalah bilangan prima");
+            }
         }
     }
 }
